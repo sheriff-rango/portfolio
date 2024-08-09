385f29c859607af794f074ef2610a2f84b36fff7
sheriff-rango <sheriff0613@gmail.com>
2024-08-09 14:34:47 -0500

    added contact form

--- js/scripts.js
+++ js/scripts.js
@@ -31,4 +31,95 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+    // Initialize EmailJS with the Public Key
 });
+
+function resetError(event) {
+    // console.log('debug reset error', event)
+    const nameElement = event.target.name;
+    const emailElement = event.target.email;
+    const phoneElement = event.target.phone;
+    const messageElement = event.target.message;
+
+    nameElement.nextElementSibling.textContent = ""
+    emailElement.nextElementSibling.textContent = ""
+    phoneElement.nextElementSibling.textContent = ""
+    messageElement.nextElementSibling.textContent = ""
+
+    nameElement.classList.remove("form-control-invalid");
+    emailElement.classList.remove("form-control-invalid");
+    phoneElement.classList.remove("form-control-invalid");
+    messageElement.classList.remove("form-control-invalid");
+}
+
+function sendEmail(event) {
+    event.preventDefault()
+    // console.log('element', event, event.target)
+    const nameElement = event.target.name;
+    const emailElement = event.target.email;
+    const phoneElement = event.target.phone;
+    const messageElement = event.target.message;
+
+    const name = nameElement.value.trim();
+    const email = emailElement.value.trim();
+    const phone = phoneElement.value.trim();
+    const message = messageElement.value;
+    let hasError = false;
+
+    if (!name) {
+        hasError = true;
+        nameElement.classList.add("form-control-invalid");
+        nameElement.nextElementSibling.textContent = "Please enter your name!"
+    }
+
+    if (!email) {
+        hasError = true;
+        emailElement.classList.add("form-control-invalid");
+        emailElement.nextElementSibling.textContent = "Please enter your email!"
+
+    }
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(email)) {
+        hasError = true;
+        emailElement.classList.add("form-control-invalid");
+        emailElement.nextElementSibling.textContent = "Invalid email address!"
+
+    }
+
+    if (phone) {
+        // const parsedPhone = libphonenumber.parsePhoneNumber(phone);
+        // console.log('debug phone number validation', parsedPhone);
+    }
+
+    if (!message) {
+        hasError = true;
+        messageElement.classList.add("form-control-invalid");
+        messageElement.nextElementSibling.textContent = "Please enter your message!"
+    }
+
+    if (hasError) {
+        setTimeout(() => {
+            nameElement.nextElementSibling.textContent = ""
+            emailElement.nextElementSibling.textContent = ""
+            phoneElement.nextElementSibling.textContent = ""
+            messageElement.nextElementSibling.textContent = ""
+
+            nameElement.classList.remove("form-control-invalid");
+            emailElement.classList.remove("form-control-invalid");
+            phoneElement.classList.remove("form-control-invalid");
+            messageElement.classList.remove("form-control-invalid");
+        }, 5000)
+        return
+    };
+
+    console.log('hereerererererer', { name, email, phone, message })
+    emailjs.send('service_rrb3ima', 'template_l7qenfz', { name, email, phone, message }).then(
+        (response) => {
+            console.log('SUCCESS!', response.status, response.text);
+        },
+        (error) => {
+            console.log('FAILED...', error);
+        },
+    )
+}
